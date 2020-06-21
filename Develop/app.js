1036b5b95e31103afdcbbfae86f4648f696529a3
John Bouchard <jbouchard13@gmail.com>
2020-06-20 23:41:02 -0400

    added intro prompts with if statements to determine what functions to run:

--- Develop/app.js
+++ Develop/app.js
@@ -1,16 +1,48 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const Database = require("./db/Database");
+const { connection } = require("./db/Database");
 
 // create a prompt when the app is started
-// this will ask the user what they'd like to do
-// choices: {
-//  {
-//      name:
-//      value:
-//  }
-// }
-Database.getAllEmployees();
+const init = async () => {
+  const intro = await inquirer.prompt({
+    type: "list",
+    name: "todo",
+    message: "What would you like to do?",
+    choices: [
+      "See all employees",
+      "See all departments",
+      "See all roles",
+      "Add new employee",
+      "Add new department",
+      "Add new role",
+      "Update employee's role",
+      "Exit",
+    ],
+  });
 
-// if view employees selected, it'll call getEmployees function
-// call the findEmployees function from the Database class that we created
+  if (intro.todo === "See all employees") {
+    // call function to retrieve all employees
+  } else if (intro.todo === "See all departments") {
+    // call function to retrieve all departments
+  } else if (intro.todo === "See all roles") {
+    // call function to retrieve all roles
+  } else if (intro.todo === "Add new employee") {
+    // call function to add new employee
+    // prompt for employee's first and last name
+    // prompt for their role_id and manager_id(can be null)
+  } else if (intro.todo === "Add new department") {
+    // call function to add new department
+    // prompt for department name
+  } else if (intro.todo === "Add new role") {
+    // call function to add new role
+    // prompt for role title, salary, and department_id
+  } else if (intro.todo === "Update employee's role") {
+    // call function to update an employee's role
+  } else if (intro.todo === "Exit") {
+    // end the connection to the db
+    connection.end();
+  }
+};
+
+init();
